a38f3614444676a0006d7f35f9334c66d4ae5ef8
FilipHlavka <113585003+FilipHlavka@users.noreply.github.com>
2024-03-16 14:51:02 +0100

    healthbar budovy

--- Assets/budovy/budova.cs
+++ Assets/budovy/budova.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class budova : MonoBehaviour
 {
@@ -13,8 +14,9 @@ public class budova : MonoBehaviour
     public PowerPointGenerator powerPointGenerator; // nièení enemy nebo èasovaèem
     public Vector2 pozice;
     public List<GameObject> enemies;
+    Slider slider;
     SpriteRenderer spriteRenderer;
-   // bool pom = true;
+   
    // BudovaCont bdvCont;
 
    
@@ -25,10 +27,11 @@ public class budova : MonoBehaviour
             if (Vector2.Distance(enemy.transform.position, transform.position) < 11)
             {
                 zivoty-= 2;
+                slider.value = zivoty;
                 Debug.Log(zivoty);
                 if (zivoty <= 0 )
                 {
-                    //pom = false;
+                    
                     BudovaCont.poziceZnicenychBudov.Add((Vector2)transform.position);
                    // Debug.Log(BudovaCont.poziceZnicenychBudov.Count() + " to je ten list");
                     Destroy(gameObject);
@@ -44,6 +47,8 @@ public class budova : MonoBehaviour
         sprite = Resources.Load<Sprite>("budovy/" + jmeno);
         //Debug.Log(sprite);
         spriteRenderer.sprite = sprite;
+        slider = gameObject.GetComponentInChildren<Slider>();
+        slider.maxValue = zivoty;
         //Debug.Log(Application.dataPath);
         StartCoroutine(pockej());
         InvokeRepeating("Checkuj", 2, 2);
