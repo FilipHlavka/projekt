284b2ead75940c530f46d0d0aa68633c85242c35
FilipHlavka <113585003+FilipHlavka@users.noreply.github.com>
2024-03-22 19:38:24 +0100

    nic moc oprava

--- Assets/scene manager/ingamemanager.cs
+++ Assets/scene manager/ingamemanager.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ingamemanager : MonoBehaviour
 {
     soubojContainer controller;
-    public static int l=0;
+    public static int l = 0;
+    List<GameObject> entity = new List<GameObject>();
     void Start()
     {
         
@@ -14,11 +16,22 @@ public class ingamemanager : MonoBehaviour
 
     public void PrepniNascenu(string scena)
     {
-        //Debug.Log(soubojContainer.enemy+" "+soubojContainer.hrac+" "+soubojContainer.enemyNaTahu);
-        
-            SceneManager.LoadScene(scena);
+        // Zapnout fake loading screen!!!!!!!!!!!!!!!!!!!!! >w<
+        ZnicTyZatracenyObjekty();
+        SceneManager.LoadScene(scena);
 
         
+    }
+    private void ZnicTyZatracenyObjekty()
+    {
+
+        entity = GameObject.FindGameObjectsWithTag("enemy").ToList();
+        entity.Add(GameObject.FindGameObjectWithTag("Player"));
+
+        foreach (GameObject mrchaneznicitelna in entity)
+        {
+            Destroy(mrchaneznicitelna);
+        }
     }
     void Update()
     {
