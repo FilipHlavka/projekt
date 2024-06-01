b44acfec39b392e0a26e428ec1ca052e241ef544
FilipHlavka <113585003+FilipHlavka@users.noreply.github.com>
2024-06-01 19:25:10 +0200

    uprava enemy respawnu

--- Assets/prefs/Enemies/enemyScripts/EnemyRespawn.cs
+++ Assets/prefs/Enemies/enemyScripts/EnemyRespawn.cs
@@ -2,6 +2,7 @@ using FOVMapping;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.Events;
 
 public class EnemyRespawn : MonoBehaviour
@@ -47,12 +48,34 @@ public class EnemyRespawn : MonoBehaviour
         float z = Random.Range(spawnBounds.min.z, spawnBounds.max.z) + SpawnPlocha.transform.position.z;
         float y = SpawnPlocha.SampleHeight(new Vector3(x, 0, z)) + SpawnPlocha.transform.position.y;
 
-        GameObject enemy = Instantiate(
-            enemies.enemies[Random.Range(0,enemies.enemies.Count)].enemak.gameObject,
-            new Vector3(x,y,z), 
+        Vector3 pozice = new Vector3(x, y, z);
+
+        if (Checkni(pozice, out Vector3 fajnPozice))
+        {
+            GameObject enemy = Instantiate(
+            enemies.enemies[Random.Range(0, enemies.enemies.Count)].enemak.gameObject,
+            new Vector3(x, y, z),
             Quaternion.Euler(0, Random.Range(0, 360), 0));
-        enemyRespawn.Invoke(enemy);
-        FOVManager.instance.FindAllFOVAgents();
+            enemyRespawn.Invoke(enemy);
+            FOVManager.instance.FindAllFOVAgents();
+        }
+        else
+        {
+           Spawn();
+        }
+        
+    }
+
+    private bool Checkni(Vector3 pozice, out Vector3 nova)
+    {
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(pozice, out hit, 10, NavMesh.AllAreas))
+        {
+            nova = hit.position;
+            return true;
+        }
+        nova = Vector3.zero;
+        return false;
     }
 
     IEnumerator SpawnCoroutine()
@@ -61,7 +84,7 @@ public class EnemyRespawn : MonoBehaviour
         
         while (spawnuj)
         {
-            yield return new WaitForSeconds(25f);
+            yield return new WaitForSeconds(30f);
             if(spawnuj)
             Spawn();
 
